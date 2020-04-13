a9fd43938206d709eab53aaf164db3c73671ae0e
Jack Ward <jward8@wisc.edu>
2020-04-12 22:20:01 -0500

    Still testing, makes list of unpopular songs
    Goes through saved songs library and saves songs that have popularity less than 40.

--- main1.py
+++ main1.py
@@ -19,6 +19,36 @@ app_token = request_client_token(client_id,client_secret)
 spotify = Spotify(app_token)
 spotify.token = user_token
 
-tracks = spotify.current_user_top_tracks(limit=20)
-for track in tracks.items:
-    print(track.name)
+current_user = Spotify.current_user(spotify)
+user_id = current_user.id
+print(user_id)
+
+# track_id = []
+# tracks = spotify.current_user_top_tracks(limit=20)
+# for track in tracks.items:
+#     track_id.append(track.id)
+for set in range(0,1000,20):
+    library = spotify.saved_tracks(None,limit=20,offset=set).items
+    track_list = []
+    for track in library:
+        track_item = track.track
+        track_list.append(track_item)
+
+    not_pop = []
+    for track in track_list:
+        if track.popularity <= 40:
+            not_pop.append(track)
+
+    for track in not_pop:
+        count = 0
+        artists_name = ""
+        for artists in track.artists:
+            n = artists.name
+            if count == 0:
+                artists_name = n
+                count += 1
+                continue
+            artists_name = artists_name + " & " + n
+        print(track.name + " by " + artists_name) 
+
+# post = spotify.playback_currently_playing
