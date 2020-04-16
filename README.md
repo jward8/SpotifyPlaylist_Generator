bce1443b860fd1b562caf5023f7628a61372831d
Jack Ward <jward8@wisc.edu>
2020-04-16 09:27:42 -0500

    updated main1 and README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # SpotifyPlaylist_Generator
+Program that intends to generate a daily playlist on Spotify for user.
+
+## Project Status
+In mostly testing mode, still trying out the tekore library to understand full capabilities.
+
+## Requirements
+(For Now):Tekore
+```bash
+pip install tekore
+```
+More may be added later
+
+## Contributing
+Purpose of this project is to learn Spotify API and tekore library. 
+So For now, no pull requests
+
+## License
+https://img.shields.io/github/license/jward8/spotifyplaylist_generator
--- main1.py
+++ main1.py
@@ -1,54 +1,65 @@
 from tekore.util import request_client_token
 from tekore import Spotify
-from tekore.scope import every
+from tekore.scope import read
 from tekore.util import prompt_for_user_token
+import tekore
 
-client_id = 'd217fc4b5c4540d0bf2299a6c5a09c9a'
-client_secret = '504db230bc054da896f2c442e1a1f890'
-redirect_uri = 'https://google.com/'
-
-user_token = prompt_for_user_token(
-    client_id,
-    client_secret,
-    redirect_uri,
-    scope=every
-)
-
-app_token = request_client_token(client_id,client_secret)
-
-spotify = Spotify(app_token)
-spotify.token = user_token
-
-current_user = Spotify.current_user(spotify)
-user_id = current_user.id
-print(user_id)
-
-# track_id = []
-# tracks = spotify.current_user_top_tracks(limit=20)
-# for track in tracks.items:
-#     track_id.append(track.id)
-for set in range(0,1000,20):
-    library = spotify.saved_tracks(None,limit=20,offset=set).items
+def non_pop_list(spotify):
     track_list = []
-    for track in library:
-        track_item = track.track
-        track_list.append(track_item)
+    for set in range(0,40,20):
+        library = spotify.saved_tracks(None,limit=20,offset=set).items
+        for track in library:
+            track_item = track.track
+            if track_item.popularity <= 40:
+                track_list.append(track_item)
+    return track_list
 
-    not_pop = []
-    for track in track_list:
-        if track.popularity <= 40:
-            not_pop.append(track)
+def non_pop_recommended(track_list, spotify):
+    top_5 = []
+    reco_list = []
+    for set in range(0,4):
+        top_5.append(track_list[set])
+    id_list = [track.id for track in top_5]
+    recommended = spotify.recommendations(track_ids=id_list).tracks
+    for track in recommended:
+        reco_list.append(track)
+    return reco_list
 
-    for track in not_pop:
-        count = 0
-        artists_name = ""
-        for artists in track.artists:
-            n = artists.name
-            if count == 0:
-                artists_name = n
-                count += 1
-                continue
-            artists_name = artists_name + " & " + n
-        print(track.name + " by " + artists_name) 
+def set_up():
+    client_id = 'd217fc4b5c4540d0bf2299a6c5a09c9a'
+    client_secret = '504db230bc054da896f2c442e1a1f890'
+    redirect_uri = 'https://google.com/'
+
+    user_token = prompt_for_user_token(
+        client_id,
+        client_secret,
+        redirect_uri,
+        scope= read
+    )
+
+    app_token = request_client_token(client_id,client_secret)
+
+    spotify = Spotify(app_token)
+    spotify.token = user_token
+
+    track_list = non_pop_list(spotify)
+    recommended = non_pop_recommended(track_list,spotify)
+
+    for track in recommended:
+        artists = track.artists
+        name = ""
+        for artist in artists:
+            if len(artists) == 1:
+                name = artist.name
+                break
+            name += " & " + artist.name
+        print(track.name + " by " + name)
+    return 
+
+if __name__ == "__main__":
+    set_up()
+
+
+
 
 # post = spotify.playback_currently_playing
